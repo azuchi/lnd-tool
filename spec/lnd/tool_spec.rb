41848010df1a20c7ed05ef85d7bd1eec96ece779
azuchi <azuchi@chaintope.com>
2021-11-22 22:16:22 +0900

    Fix typo

--- spec/lnd/tool_spec.rb
+++ spec/lnd/tool_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Lnd::Tool do
-  it "has a version number" do
-    expect(Lnd::Tool::VERSION).not_to be nil
+RSpec.describe LND::Tool do
+  it 'has a version number' do
+    expect(LND::Tool::VERSION).not_to be nil
   end
 end
